5846fca7bd8cb6bc74e920795d9e813f252784fd
Антон-ПК <a.v.rtkunov@bk.ru>
2022-02-20 22:06:40 +0300

    Version 1.0

--- src/Test1.java
+++ src/Test1.java
@@ -7,7 +7,9 @@ public class Test1 {
         map.put(new Employee("Anton", 22), "first");
         map.put(new Employee("Anton", 21), "second");
         map.put(new Employee("Anton", 23), "third");
-
+        map.put(new Employee("Anton", 26), "first");
+        map.put(new Employee("Anton", 27), "second");
+        map.put(new Employee("Anton", 24), "third");
         System.out.println(map.size());
     }
 }
